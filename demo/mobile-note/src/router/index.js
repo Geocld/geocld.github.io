53244a70e302c9fc4085d965f8ae08050a96006a
lijiahao <375822331@qq.com>
2016-04-30 22:01:24 +0800

    edit mobile demo

--- demo/mobile-note/src/router/index.js
+++
@@ -1,4 +0,0 @@
-/*路由配置*/
-export default function(router) {
-	
-}
--- demo/mobile-note/src/store/temp_store.js
+++
@@ -1,9 +0,0 @@
-//全局变量，便于路由和路由之间传递数据
-export const tmp = {
-	note: {
-		id: '',
-		title: '',
-		content: '',
-		category: ''
-	}
-}
